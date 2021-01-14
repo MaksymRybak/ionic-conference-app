1e47b6425a3f2ed7d10a42e92c014aeebf65faae
Maksym Rybak <rybak.maksym@gmail.com>
2021-01-14 16:43:25 +0100

    studio della pagina tutorial

--- src/app/pages/tutorial/tutorial.ts
+++ src/app/pages/tutorial/tutorial.ts
@@ -1,45 +1,49 @@
-import { Component, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
 
-import { MenuController, IonSlides } from '@ionic/angular';
+import { MenuController, IonSlides } from "@ionic/angular";
 
-import { Storage } from '@ionic/storage';
+import { Storage } from "@ionic/storage";
 
 @Component({
-  selector: 'page-tutorial',
-  templateUrl: 'tutorial.html',
-  styleUrls: ['./tutorial.scss'],
+  selector: "page-tutorial",
+  templateUrl: "tutorial.html",
+  styleUrls: ["./tutorial.scss"],
 })
 export class TutorialPage {
   showSkip = true;
 
-  @ViewChild('slides', { static: true }) slides: IonSlides;
+  @ViewChild("slides", { static: true }) slides: IonSlides; // esempio di come recuperare il riferimento al componente??
 
   constructor(
     public menu: MenuController,
     public router: Router,
-    public storage: Storage
+    public storage: Storage // docs:  https://ionicframework.com/docs/angular/storage#ionic-storage
   ) {}
 
   startApp() {
     this.router
-      .navigateByUrl('/app/tabs/schedule', { replaceUrl: true })
-      .then(() => this.storage.set('ion_did_tutorial', true));
+      .navigateByUrl("/app/tabs/schedule", { replaceUrl: true })
+      .then(() => this.storage.set("ion_did_tutorial", true));
   }
 
+  // docs: https://ionicframework.com/docs/api/slides
   onSlideChangeStart(event) {
-    event.target.isEnd().then(isEnd => {
+    // nascondo il pulsante quando sto visualizzando ultimo slide
+    event.target.isEnd().then((isEnd) => {
       this.showSkip = !isEnd;
     });
   }
 
+  // docs: https://ionicframework.com/docs/angular/lifecycle
   ionViewWillEnter() {
-    this.storage.get('ion_did_tutorial').then(res => {
+    this.storage.get("ion_did_tutorial").then((res) => {
       if (res === true) {
-        this.router.navigateByUrl('/app/tabs/schedule', { replaceUrl: true });
+        this.router.navigateByUrl("/app/tabs/schedule", { replaceUrl: true });
       }
     });
 
+    // docs: https://ionicframework.com/docs/api/menu
     this.menu.enable(false);
   }
 
